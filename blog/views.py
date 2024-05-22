76721d9df3ba2a9a291ccb5edd7f76e7ff71ab3e
Aditya Vikram Singh <adivik672@gmail.com>
2024-05-22 22:03:35 +0530

    Handled out of range number Pagination error

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if out of range, deliver the last page of result.
+        posts = paginator.page(paginator.num_pages)
     context = {"posts": posts}
     return render(request, "blog/post/list.html", context)
 
