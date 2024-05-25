ce7a046658c7ae01dcb806a61bb71ee345f70b57
Aditya Vikram Singh <adivik672@gmail.com>
2024-05-25 16:41:01 +0530

    Postgresql configured

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -89,8 +89,10 @@ WSGI_APPLICATION = "blog_website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": getenv("DATABASE_PASSWORD"),
     }
 }
 
